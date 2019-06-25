51d3132a2c9303d1f1a98ecefa4c578020f49919
Tyler Nijmeh <tylernij@gmail.com>
2019-06-24 17:33:11 -0700

    mm: Refactor ASLR default configuration options
    Allow maintainers to select ASLR_FULL, ASLR_PARTIAL, or ASLR_NONE. Be
    warned, this commit allows users to disable ASLR entirely in the kernel,
    which can be a critical security issue.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/elf-randomize.h
+++ include/linux/elf-randomize.h
@@ -6,7 +6,8 @@ struct mm_struct;
 
 #ifndef CONFIG_ARCH_HAS_ELF_RANDOMIZE
 static inline unsigned long arch_mmap_rnd(void) { return 0; }
-# if defined(arch_randomize_brk) && defined(CONFIG_COMPAT_BRK)
+# if defined(arch_randomize_brk) && \
+	(defined(CONFIG_ASLR_NONE) || defined(CONFIG_ASLR_PARTIAL))
 #  define compat_brk_randomized
 # endif
 # ifndef arch_randomize_brk
@@ -15,7 +16,7 @@ static inline unsigned long arch_mmap_rnd(void) { return 0; }
 #else
 extern unsigned long arch_mmap_rnd(void);
 extern unsigned long arch_randomize_brk(struct mm_struct *mm);
-# ifdef CONFIG_COMPAT_BRK
+# if defined(CONFIG_ASLR_NONE) || defined(CONFIG_ASLR_PARTIAL)
 #  define compat_brk_randomized
 # endif
 #endif
