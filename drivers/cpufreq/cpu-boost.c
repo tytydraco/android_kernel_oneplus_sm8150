b97d85f7a431873bb686ba56ff00ce9c97e5b5a1
Tyler Nijmeh <tylernij@gmail.com>
2019-08-16 13:53:00 -0700

    cpu-boost: Allow sched_boost to be omitted
    If CONFIG_SCHED_BOOST is not enabled, there is no sense in processing
    extra logic on input events for a case that can never be true.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -39,10 +39,12 @@ static bool input_boost_enabled;
 static unsigned int input_boost_ms = 250;
 module_param(input_boost_ms, uint, 0644);
 
+#ifdef CONFIG_SCHED_BOOST
 static unsigned int sched_boost_on_input;
 module_param(sched_boost_on_input, uint, 0644);
 
 static bool sched_boost_active;
+#endif
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
@@ -166,7 +168,10 @@ static void update_policy_online(void)
 
 static void do_input_boost_rem(struct work_struct *work)
 {
-	unsigned int i, ret;
+	unsigned int i;
+#ifdef CONFIG_SCHED_BOOST
+	unsigned int ret;
+#endif
 	struct cpu_sync *i_sync_info;
 
 	/* Reset the input_boost_min for all CPUs in the system */
@@ -179,24 +184,32 @@ static void do_input_boost_rem(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
+#ifdef CONFIG_SCHED_BOOST
 	if (sched_boost_active) {
 		ret = sched_set_boost(0);
 		if (ret)
 			pr_err("cpu-boost: sched boost disable failed\n");
 		sched_boost_active = false;
 	}
+#endif
 }
 
 static void do_input_boost(struct work_struct *work)
 {
-	unsigned int i, ret;
+	unsigned int i;
+#ifdef CONFIG_SCHED_BOOST
+	unsigned int ret;
+#endif
 	struct cpu_sync *i_sync_info;
 
 	cancel_delayed_work_sync(&input_boost_rem);
+
+#ifdef CONFIG_SCHED_BOOST
 	if (sched_boost_active) {
 		sched_set_boost(0);
 		sched_boost_active = false;
 	}
+#endif
 
 	/* Set the input_boost_min for all CPUs in the system */
 	pr_debug("Setting input boost min for all CPUs\n");
@@ -208,6 +221,7 @@ static void do_input_boost(struct work_struct *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
+#ifdef CONFIG_SCHED_BOOST
 	/* Enable scheduler boost to migrate tasks to big cluster */
 	if (sched_boost_on_input > 0) {
 		ret = sched_set_boost(sched_boost_on_input);
@@ -216,6 +230,7 @@ static void do_input_boost(struct work_struct *work)
 		else
 			sched_boost_active = true;
 	}
+#endif
 
 	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
 					msecs_to_jiffies(input_boost_ms));
