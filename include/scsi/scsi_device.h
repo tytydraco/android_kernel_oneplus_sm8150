8f887ae88dc4367f50502aa0987c8033ad06b0c2
Tyler Nijmeh <tylernij@gmail.com>
2019-08-15 23:04:34 -0700

    scsi: Optionally remove debug and tracing
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -244,15 +244,28 @@ struct scsi_device {
  * like scmd_printk, but the device name is passed in
  * as a string pointer
  */
+#ifdef CONFIG_DEBUG_KERNEL
 __printf(4, 5) void
 sdev_prefix_printk(const char *, const struct scsi_device *, const char *,
 		const char *, ...);
+__printf(3, 4) void
+scmd_printk(const char *, const struct scsi_cmnd *, const char *, ...);
+#else
+static inline __printf(4, 5) void
+sdev_prefix_printk(const char *level, const struct scsi_device *sdev,
+		const char *name, const char *fmt, ...)
+{
+}
+
+static inline __printf(3, 4) void
+scmd_printk(const char *level, const struct scsi_cmnd *scmd,
+		const char *fmt, ...)
+{
+}
+#endif
 
 #define sdev_printk(l, sdev, fmt, a...)				\
 	sdev_prefix_printk(l, sdev, NULL, fmt, ##a)
-
-__printf(3, 4) void
-scmd_printk(const char *, const struct scsi_cmnd *, const char *, ...);
 
 #define scmd_dbg(scmd, fmt, a...)					   \
 	do {								   \
