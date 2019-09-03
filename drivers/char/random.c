6b705f964858e1b1d71dbec5a8b7b5633b116baa
Tyler Nijmeh <tylernij@gmail.com>
2019-09-02 17:51:54 -0700

    random: Do not warn on unused random read functions
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1849,13 +1849,13 @@ _random_read(int nonblock, char __user *buf, size_t nbytes)
 	}
 }
 
-static ssize_t
+__maybe_unused static ssize_t
 random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
 	return _random_read(file->f_flags & O_NONBLOCK, buf, nbytes);
 }
 
-static ssize_t
+__maybe_unused static ssize_t
 urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
 	unsigned long flags;
