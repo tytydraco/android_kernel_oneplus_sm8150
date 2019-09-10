96ab6d835db6df35fba8ffe95b1080826f56254b
Tyler Nijmeh <tylernij@gmail.com>
2019-09-09 18:07:35 -0700

    block: Force requests onto their origin CPU
    There is a high probability that the specific CPU that makes an I/O
    request may be repeated. For example, the same CPU may read the same
    data multiple times when reading a database. This flag forces requests
    to be completed on the same CPU that requested them. Additionally, this
    prevents a scenario where one CPU is pinned with handling multiple block
    requests from other CPUs. Since we have an 8 core system, we are in no
    shortage of available CPUs to handle block requests.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -664,10 +664,12 @@ struct request_queue {
 #define QUEUE_FLAG_INLINECRYPT 29	/* inline encryption support */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
-				 (1 << QUEUE_FLAG_SAME_COMP)
+				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
-				 (1 << QUEUE_FLAG_SAME_COMP)
+				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE))
 
 /*
  * @q->queue_lock is set while a queue is being initialized. Since we know
