7d20cbc62893692212763177218575a12e7ac26e
Tyler Nijmeh <tylernij@gmail.com>
2020-02-01 00:15:20 -0800

    treewide: Allow compilation without MEMPLUS
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1054,10 +1054,11 @@ static int __set_oom_adj(struct file *file, int oom_adj, bool legacy)
 		}
 	}
 
+#ifdef CONFIG_MEMPLUS
 	/* CONFIG_MEMPLUS add start by bin.zhong@ATSI */
 	memplus_state_check(legacy, oom_adj, task, 0, 0);
 	/* add end */
-
+#endif
 	task->signal->oom_score_adj = oom_adj;
 #ifdef CONFIG_ADJ_CHAIN
 	adj_chain_update_oom_score_adj(task);
