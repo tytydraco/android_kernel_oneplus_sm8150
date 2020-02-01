fd82ea59c0cffa92ff5189bf4cf0c390b32bb728
Tyler Nijmeh <tylernij@gmail.com>
2020-02-01 08:31:35 -0800

    oem_force_dump: Allow compilation without driver
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/oem_force_dump.h
+++ include/linux/oem_force_dump.h
@@ -9,7 +9,12 @@
 #ifndef OEM_FORCE_DUMP_H
 #define OEM_FORCE_DUMP_H
 
+#ifdef CONFIG_OEM_FORCE_DUMP
 extern void oem_check_force_dump_key(unsigned int code, int value);
+#else
+static inline void oem_check_force_dump_key(unsigned int code, int value) {}
+#endif
+
 extern int oem_get_download_mode(void);
 void send_msg(char *message);
 int  msm_serial_oem_init(void);
